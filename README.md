00fd9f95e4e320e9913ede471c1309bfae01c8db
Jens Petersen <petersen@redhat.com>
2021-07-02 19:25:42 +0800

    initial commit with compiler, list, and pkg commands

---
+++ README.md
@@ -0,0 +1,16 @@
+# pantry-cli
+
+## Usage
+
+```
+$ pantry-cli ghc lts-18.0
+ghc-8.10.4
+$ pantry-cli list lts-17.15 | wc -l
+2644
+$ pantry-cli pkg lts-16.31 base
+pantry-0.4.0.2
+```
+
+## Installation
+
+Run `stack install` or `cabal install`
---
+++ src/Main.hs
@@ -0,0 +1,61 @@
+-- SPDX-License-Identifier: BSD-3-Clause
+
+module Main (main) where
+
+import qualified Data.Map as M
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import Distribution.Pretty
+import Distribution.Types.PackageName
+import Pantry
+import RIO (textDisplay)
+import SimpleCmd
+import SimpleCmdArgs
+
+data Command = Compiler | List
+
+main :: IO ()
+main = do
+  simpleCmdArgs Nothing "Pantry CLI" "Query Stackage snapshots with pantry" $
+    subcommands
+    [ Subcommand "compiler" "Get compiler version of snapshot" $
+      runCmd Compiler <$> strArg "SNAPSHOT"
+    , Subcommand "list" "List snapshot packages" $
+      runCmd List <$> strArg "SNAPSHOT"
+    , Subcommand "pkg" "Show snapshot package" $
+      showPackage <$> strArg "SNAPSHOT" <*> strArg "PACKAGE"
+    ]
+
+-- FIXME handle snapshot aliases
+getSnapshot :: T.Text -> IO RawSnapshot
+getSnapshot snap =
+  runPantryApp $ do
+  snapshot <- parseSnapName $ snap
+  let rurl = defaultSnapshotLocation snapshot
+  url <- completeSnapshotLocation rurl
+  loadSnapshot url
+
+runCmd :: Command -> T.Text -> IO ()
+runCmd com snap = do
+  rsnap <- getSnapshot snap
+  case com of
+    Compiler -> T.putStrLn $ textDisplay $ rsCompiler rsnap
+    List -> do
+      let pkgmap = rsPackages rsnap
+      mapM_ (putStrLn . packageVerId) $ M.elems pkgmap
+
+packageVerId :: RawSnapshotPackage -> String
+packageVerId rsp =
+  case rspLocation rsp of
+    RPLIHackage (PackageIdentifierRevision n v _) _ ->
+      prettyShow (PackageIdentifier n v)
+    _ -> error "non-Hackage package"
+
+showPackage :: T.Text -> String -> IO ()
+showPackage snap pkg = do
+  rsnap <- getSnapshot snap
+  let pkgmap = rsPackages rsnap
+  return ()
+  case M.lookup (mkPackageName pkg) pkgmap of
+    Just rsp -> putStrLn (packageVerId rsp)
+    Nothing -> error' $ pkg ++ " not found in " ++ T.unpack snap
